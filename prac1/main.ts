38c837df20b8bae586cc8ac45dcfe1b127a91804
gdomondon <gadomondon@up.edu.ph>
2025-04-15 23:51:11 +0700

    built root, added button that creates new counters. Counters exist if created, but are currently not functional.

---
+++ prac1/main.ts
@@ -0,0 +1,44 @@
+import { Array } from 'effect'
+
+function main() {
+
+    function addNewCounter() {
+        const incrButton = document.createElement('button')
+        const counterDisplay = document.createElement('p')
+        const decrButton = document.createElement('button')
+        const deleteButton = document.createElement('button')
+
+        const children = Array.make(incrButton, counterDisplay, decrButton, deleteButton)
+
+        let counter: number = 0
+
+        incrButton.textContent = '+'
+        decrButton.textContent = '-'
+        deleteButton.textContent = 'Delete'
+        counterDisplay.textContent = `${counter}`
+
+        const counterNode = document.createElement('div')
+        
+        for (const element of children) {
+            counterNode.appendChild(element)
+        }
+
+        root.appendChild(counterNode)
+        
+        
+    }
+
+
+    const root = document.querySelector('#root')! // root explicitly defined in html file
+
+    const newCounterButton = document.createElement('button')
+    newCounterButton.textContent = 'New Counter'
+
+    newCounterButton.addEventListener('click', addNewCounter)
+
+    root.appendChild(newCounterButton)
+
+
+}
+
+main()
