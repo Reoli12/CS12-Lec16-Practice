da48b4f3fc5c3c5a5f3673e11ed7354236f9358d
gdomondon <gadomondon@up.edu.ph>
2025-04-16 08:06:19 +0700

    works

--- prac1/main.ts
+++ prac1/main.ts
@@ -3,6 +3,8 @@ import { Array } from 'effect'
 function main() {
 
     function addNewCounter() {
+        // adds a node to root, and all elements that compose a counter depends on that node
+
         const incrButton = document.createElement('button')
         const counterDisplay = document.createElement('p')
         const decrButton = document.createElement('button')
@@ -22,6 +24,22 @@ function main() {
         for (const element of children) {
             counterNode.appendChild(element)
         }
+
+        // event listeners
+
+        incrButton.addEventListener('click', () => {
+            counter++
+            counterDisplay.textContent = `${counter}`
+        })
+
+        decrButton.addEventListener('click', () => {
+            counter--
+            counterDisplay.textContent = `${counter}`
+        })
+
+        deleteButton.addEventListener('click', () => {
+            root.removeChild(counterNode)
+        })
 
         root.appendChild(counterNode)
         
